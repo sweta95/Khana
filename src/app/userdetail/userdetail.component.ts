5fc3e7e873e7cb7aca8804077cefe6df3ae388b8
ssi112 <ssingh112@sapient.com>
2019-02-01 17:47:51 +0530

    done

--- src/app/userdetail/userdetail.component.ts
+++ src/app/userdetail/userdetail.component.ts
@@ -2,15 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-userdetail',
-  template: `
-    <p>
-      userdetail works!
-    </p>
-  `,
-  styles: []
+  templateUrl: './userdetail.component.html',
+  styles: [`.example-container {
+    display: flex;
+    flex-direction: column;
+    background-color:white;
+    padding:20px;
+  }
+  .example-container > * {
+    width: 100%;
+  }`]
 })
 export class UserdetailComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit() {
